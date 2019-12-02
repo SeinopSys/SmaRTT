f9c3c348a02d9cc3753eaee70070b629f2c70808
Zoltan Dalmadi <dmz985@gmail.com>
2019-12-02 09:43:52 +0100

    add logo change snippet

--- extension/js/content.js
+++ extension/js/content.js
@@ -3,3 +3,10 @@ const sendToken = () =>
     void resp || setTimeout(sendToken, 2000)
   );
 sendToken();
+
+$(() => {
+  requestAnimationFrame(() => {
+    $('#heading > span.name').html('SmarRTT');
+    $('#heading > img').attr('src', 'https://cdn3.iconfinder.com/data/icons/gray-user-toolbar/512/manager-512.png');
+  });
+});
